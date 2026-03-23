b8f340616294b79b44e18da5b19fae817fc4350e
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-03-23 08:50:38 -0500

    Enhance UI with input and button for guessing
    Added input field and button for user interaction.

--- sketch.js
+++ sketch.js
@@ -1,12 +1,22 @@
+// Global Variables
+let  myInput;
+let myButton;
+
 function setup() {
   createCanvas(400, 400);
+  myInput = createInput("Guess a Number");
+  myInput.position(20,150)
+  myButton = createButton('guess number');
+  myButton.position(20, 175)
 }
 
 function draw() {
   background("#003049");
-  fill("red")
-  rect(20,20,360,100)
-  fill("#FDF0D5")
-  textSize(24)
-  text("High Low Game",100,50)
+  fill("red");
+  rect(20,20,360,100);
+  fill("#FDF0D5");
+  textSize(24);
+  text("High Low Game",100,50);
+  textSize(14);
+  text("I have thought of a number between 1-100",20,140);
 }
