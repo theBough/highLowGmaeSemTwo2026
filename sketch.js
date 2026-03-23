00b102d5706b59e68d8411d135a547f531e2c478
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-03-23 09:02:49 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,16 +2,19 @@
 let  myInput;
 let myButton;
 
+//------- below are setup and draw functions----------------
+//------- They ALWAYS have to be there --------------------
 function setup() {
   createCanvas(400, 400);
   myInput = createInput("Guess a Number");
   myInput.position(20,150)
   myButton = createButton('guess number');
   myButton.position(20, 175)
+  myButton.mousePressed(submitGuess)
 }
-
 function draw() {
   background("#003049");
+  //cursor(ARROW)
   fill("red");
   rect(20,20,360,100);
   fill("#FDF0D5");
@@ -19,4 +22,13 @@ function draw() {
   text("High Low Game",100,50);
   textSize(14);
   text("I have thought of a number between 1-100",20,140);
+}
+//--------------- The Functions below are ones that I made------
+function submitGuess(){
+  //Go and get what is inside the input box, and save it into
+  //a variable called their guess.
+  let theirGuess = myInput.value();
+  //console.log is a nice way to 'see under the hood'
+  console.log(theirGuess)
+  
 }
